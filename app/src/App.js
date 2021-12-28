093d008a43c0f5f9e2b28faa8ba308784d49d6f4
immacio <mauricewillis44@gmail.com>
2021-12-28 16:53:07 +0800

    UI updates

--- app/src/App.js
+++ app/src/App.js
@@ -110,7 +110,7 @@ const App = () => {
       </div>
       <div className = "container">
         <div className="gif-container">
-          <p className="header">The Collection</p>
+          <p className="header">🖼️ The Collection</p>
           <p className="sub-text">20 unique NFTs available available now on <a href="https://solsea.io/collection/61bf063ebe82aad29715c1df">SolSea</a></p>
           <div className="gif-grid">
             <div className="gif-item">
